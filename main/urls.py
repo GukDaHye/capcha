ec3ad5c80fdc177deae3724ddf545ad1ba47a056
GukDaHye <dahye.guk@gmail.com>
2024-12-15 20:52:40 +0900

    add:dashboard.html & dashboard_map.html
    - Verified URL patterns for 'main:dashboard' and 'main:dashboard_map'.

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main.views import DashboardView
+from main.views import DashboardView, DashboardMapView
 
 app_name = 'main'
 
 urlpatterns = [
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),  # 'dashboard' 이름 부여
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('dashboard/map/', DashboardMapView.as_view(), name='dashboard_map'),
 ]
--- main/views.py
+++ main/views.py
@@ -13,10 +13,17 @@ class IndexView(TemplateView):
 
 
 class DashboardView(TemplateView):
-    template_name = 'main/dashboard.html'  # 템플릿 경로 지정
+    template_name = 'main/dashboard.html'
 
     def get_context_data(self, **kwargs):
-        # 템플릿에 전달할 데이터 정의
         context = super().get_context_data(**kwargs)
         context['congestion_data'] = CongestionData.objects.select_related('stop').order_by('-timestamp')[:10]
         return context
+
+class DashboardMapView(TemplateView):
+    template_name = 'main/dashboard_map.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     # context['congestion_data'] = CongestionData.objects.select_related('stop').order_by('-timestamp')[:10]
+    #     return context
